eb51043dc1ef9c35cf8b4a30948f38a2ac0587df
Chamara <chamara@weizac.com>
2024-10-29 15:31:34 +0530

    fixed bugs cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { sequelize } from './models/index.js';
 import Routes from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
-
+app.use(cors());
 app.use('/api', Routes);
 
 sequelize.sync()
